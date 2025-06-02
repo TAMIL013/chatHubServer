e2b0db455f80c71830a9d451ef5496ab1709d19d
Tamilarasu <55612299+TAMIL013@users.noreply.github.com>
2025-06-02 20:14:54 +0530

    add deepseek free model in openRouterService.js

--- services/openRouterService.js
+++ services/openRouterService.js
@@ -53,7 +53,7 @@ export function streamOpenRouter(messages, apiKey, res, chatId) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ 
-      "model": "openai/gpt-3.5-turbo",
+      "model": "deepseek/deepseek-r1-0528-qwen3-8b:free",
       "messages": messages,
       "stream": true
     }),
